6ee5d8530e469b5c70f7385df9478427243165ce
HysteriaKa <karine2310@gmail.com>
2021-04-08 12:00:24 +0200

    entity Questions

---
+++ src/Entity/Questions.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=QuestionsRepository::class)
+ */
+class Questions
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $points;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $id_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPoints(): ?int
+    {
+        return $this->points;
+    }
+
+    public function setPoints(?int $points): self
+    {
+        $this->points = $points;
+
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setIdUser(int $id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+}
